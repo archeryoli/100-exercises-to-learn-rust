fb4e3ef86f0ba8803aa4718573fa017b38c39eb5
Oliver Brandstetter <oliver.brandstetter2506@gmail.com>
2024-08-18 16:33:24 +0200

    01 - 03

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,13 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    let mut result = 1;
+    let mut runner = n;
+    while runner > 1 {
+        result *= runner;
+        runner -= 1;
+    }
+    result
 }
 
 #[cfg(test)]
